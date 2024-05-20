10b8cd071f336121155a5d2f452b48a369475af3
Karl Higley <kmhigley@gmail.com>
2024-05-20 11:17:26 -0400

    Add `Entity` and `Mention` classes to package exports

--- src/poprox_concepts/__init__.py
+++ src/poprox_concepts/__init__.py
@@ -1,5 +1,11 @@
 from poprox_concepts.account import Account
-from poprox_concepts.article import Article
+from poprox_concepts.article import Article, Entity, Mention
 from poprox_concepts.click_history import ClickHistory
 
-__all__ = ["Account", "Article", "ClickHistory"]
+__all__ = [
+    "Account",
+    "Article",
+    "Entity",
+    "Mention",
+    "ClickHistory",
+]
