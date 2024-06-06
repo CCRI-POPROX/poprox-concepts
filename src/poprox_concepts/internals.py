a9e5b442c48cfb306fa8d698fc94253cdb87c7de
Daniel Kluver <kluve018@umn.edu>
2024-06-06 11:42:52 -0500

    added hmac functions and tests

--- src/poprox_concepts/internals.py
+++ src/poprox_concepts/internals.py
@@ -1,3 +1,7 @@
+import base64
+import hashlib
+import hmac
+from typing import Optional, Type
 from uuid import UUID
 
 from pydantic import BaseModel
@@ -13,3 +17,27 @@ class Tracking_Link_Data(BaseModel):
 class Unsubscribe_Link_Data(BaseModel):
     account_id: UUID
     newsletter_id: UUID = None
+
+
+def to_hashed_base64(data: BaseModel, key: str) -> str:
+    raw_data = data.model_dump_json().encode("UTF-8")
+    raw_signature = hmac.digest(key.encode("UTF-8"), raw_data, hashlib.sha256)
+    b64_data = base64.urlsafe_b64encode(raw_data).decode("UTF-8")
+    b64_signature = base64.urlsafe_b64encode(raw_signature).decode("UTF-8")
+    return b64_data + "." + b64_signature
+
+
+def from_hashed_base64(
+    raw: str, key: str, model: Type[BaseModel]
+) -> Optional[BaseModel]:
+    if "." not in raw:
+        raise ValueError("data could not be parsed")
+    else:
+        b64_data, b64_signature = raw.split(".", 1)
+        raw_data = base64.urlsafe_b64decode(b64_data.encode("utf-8"))
+        raw_signature = base64.urlsafe_b64decode(b64_signature.encode("utf-8"))
+        expected_signature = hmac.digest(key.encode("UTF-8"), raw_data, hashlib.sha256)
+        if hmac.compare_digest(raw_signature, expected_signature):
+            return model.model_validate_json(raw_data.decode("utf-8"))
+        else:
+            raise ValueError("invalid hmac signature")
---
+++ tests/test_internals.py
@@ -0,0 +1,62 @@
+import uuid
+import base64
+import pytest
+
+from poprox_concepts.internals import (
+    Tracking_Link_Data,
+    to_hashed_base64,
+    from_hashed_base64,
+)
+
+
+def test_encode_decode():
+    key = "thisisanexamplekey"
+    data = Tracking_Link_Data(
+        newsletter_id=uuid.uuid4(),
+        account_id=uuid.uuid4(),
+        article_id=uuid.uuid4(),
+        url="https://test.com",
+    )
+    encoded = to_hashed_base64(data, key)
+    decoded = from_hashed_base64(encoded, key, Tracking_Link_Data)
+    assert decoded == data
+
+
+def test_without_hmac():
+    key = "thisisanexamplekey"
+    data = Tracking_Link_Data(
+        newsletter_id=uuid.uuid4(),
+        account_id=uuid.uuid4(),
+        article_id=uuid.uuid4(),
+        url="https://test.com",
+    )
+    encoded = base64.urlsafe_b64encode(data.model_dump_json().encode("UTF-8")).decode(
+        "UTF8"
+    )
+    with pytest.raises(ValueError):
+        from_hashed_base64(encoded, key, Tracking_Link_Data)
+
+
+def test_wrong_hmac():
+    key = "thisisanexamplekey"
+    data = Tracking_Link_Data(
+        newsletter_id=uuid.uuid4(),
+        account_id=uuid.uuid4(),
+        article_id=uuid.uuid4(),
+        url="https://test.com",
+    )
+    encoded = to_hashed_base64(data, "thisisthewrongkey")
+    with pytest.raises(ValueError):
+        from_hashed_base64(encoded, key, Tracking_Link_Data)
+
+
+def test_invalid_data_fails():
+    key = "thisisanexamplekey"
+    with pytest.raises(ValueError):
+        from_hashed_base64("blah", key, Tracking_Link_Data)
+
+
+def test_empty_string_fails():
+    key = "thisisanexamplekey"
+    with pytest.raises(ValueError):
+        from_hashed_base64("", key, Tracking_Link_Data)
