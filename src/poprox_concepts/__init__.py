1f9fd041678317e8345fcd6afb15cf9a7aba717a
Karl Higley <kmhigley@gmail.com>
2024-06-11 09:19:01 -0400

    Move domain concepts into a sub-package

--- src/poprox_concepts/__init__.py
+++ src/poprox_concepts/__init__.py
@@ -1,6 +1,6 @@
-from poprox_concepts.account import Account
-from poprox_concepts.article import Article, Entity, Mention
-from poprox_concepts.click_history import ClickHistory
+from poprox_concepts.domain.account import Account
+from poprox_concepts.domain.article import Article, Entity, Mention
+from poprox_concepts.domain.click_history import ClickHistory
 
 __all__ = [
     "Account",
