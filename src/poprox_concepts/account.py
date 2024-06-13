c3bfe57b9ea14eb445e0fcb3f45f0a2fbc82b603
Daniel Kluver <kluve018@umn.edu>
2024-06-13 11:04:20 -0500

    add account interest

--- src/poprox_concepts/account.py
+++ src/poprox_concepts/account.py
@@ -6,3 +6,10 @@ from pydantic import BaseModel
 class Account(BaseModel):
     account_id: UUID = None
     email: str
+
+
+class Account_Interest(BaseException):
+    account_id: UUID = None
+    entity_id: UUID
+    preference: int
+    frequency: int
