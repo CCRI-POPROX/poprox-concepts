ae5e14c0b0685c71af255cd76eecd42f5ce77942
Daniel Kluver <kluve018@umn.edu>
2024-06-13 11:21:16 -0500

    add name to Account_Interest

---
+++ src/poprox_concepts/account.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Account(BaseModel):
+    account_id: UUID = None
+    email: str
+
+
+class Account_Interest(BaseException):
+    account_id: UUID = None
+    entity_id: UUID
+    entity_name: str
+    preference: int
+    frequency: int
