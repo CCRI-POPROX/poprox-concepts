ff990eeb9a88cea9a77ac0b482fe377697d32f8a
Daniel Kluver <kluve018@umn.edu>
2024-06-13 11:26:31 -0500

    add AccountInterest class to __init__.py

--- src/poprox_concepts/domain/__init__.py
+++ src/poprox_concepts/domain/__init__.py
@@ -1,11 +1,5 @@
-from poprox_concepts.domain.account import Account
+from poprox_concepts.domain.account import Account, AccountInterest
 from poprox_concepts.domain.article import Article, Entity, Mention
 from poprox_concepts.domain.click_history import ClickHistory
 
-__all__ = [
-    "Account",
-    "Article",
-    "Entity",
-    "Mention",
-    "ClickHistory",
-]
+__all__ = ["Account", "Article", "Entity", "Mention", "ClickHistory", "AccountInterest"]
