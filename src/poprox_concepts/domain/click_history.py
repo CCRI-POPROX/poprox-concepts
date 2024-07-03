10ebba4d7e742c288aaee789e2b9299c8cd11faf
Karl Higley <kmhigley@gmail.com>
2024-07-03 12:47:29 -0400

    Make `ClickHistory.account_id` truly optional

--- src/poprox_concepts/domain/click_history.py
+++ src/poprox_concepts/domain/click_history.py
@@ -1,9 +1,8 @@
-from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
 class ClickHistory(BaseModel):
-    account_id: UUID = None
-    article_ids: List[UUID]
+    account_id: UUID | None = None
+    article_ids: list[UUID]
