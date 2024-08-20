e3236b2dc2f833ba1e3598de71f84745fdf41dca
Karl Higley <kmhigley@gmail.com>
2024-08-20 10:18:08 -0400

    Create and apply a `Newsletter` domain concept

--- src/poprox_concepts/domain/__init__.py
+++ src/poprox_concepts/domain/__init__.py
@@ -2,16 +2,18 @@ from poprox_concepts.domain.account import Account, AccountInterest
 from poprox_concepts.domain.article import Article, ArticleSet, Entity, Mention
 from poprox_concepts.domain.click import Click
 from poprox_concepts.domain.image import Image
+from poprox_concepts.domain.newsletter import Newsletter
 from poprox_concepts.domain.profile import InterestProfile
 
 __all__ = [
     "Account",
+    "AccountInterest",
     "Article",
     "ArticleSet",
+    "Click",
     "Entity",
     "Image",
-    "Mention",
-    "Click",
-    "AccountInterest",
     "InterestProfile",
+    "Mention",
+    "Newsletter",
 ]
