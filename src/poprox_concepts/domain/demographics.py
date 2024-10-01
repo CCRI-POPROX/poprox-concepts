a743b55fbf4a82a7b07cc983a6084d4fed98ffce
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2024-10-01 00:10:44 -0400

    added demographics base models

---
+++ src/poprox_concepts/domain/demographics.py
@@ -0,0 +1,45 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Demographics(BaseModel):
+    demographic_id: UUID = None
+    account_id: UUID
+    gender: str
+    birth_year: int
+    zip5: str
+    education: str
+    race: str
+    
+
+GENDER_OPTIONS = [
+    'Woman', 
+	'Man',
+	'Non-binary', 
+	'Other', 
+	'Prefer not to say']
+
+
+EDUCATION_OPTIONS = [
+	'Some high school', 
+	'High school',
+	'Some college',
+	'Trade, technical or vocational training',
+	'Associate\'s degree',
+	'Bachelor\'s degree',
+	'Master\'s degree',
+	'Professional degree',
+	'Doctorate',
+	'Prefer not to say']
+    
+
+RACE_OPTIONS = [
+	'White',
+	'Black or African American',
+	'American Indian or Alaska Native',
+	'Asian',
+    'Native Hawaiian or Other Pacific Islander',
+	'Prefer not to say',
+	'Not listed (please specify)']
+
