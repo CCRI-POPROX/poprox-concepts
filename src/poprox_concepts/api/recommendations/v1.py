0de93a165a26192325af393a19c01d9150cfd7fd
Karl Higley <khigley@umn.edu>
2025-02-28 11:46:47 -0500

    Create separate Pydantic models for versions of the API/protocol

---
+++ src/poprox_concepts/api/recommendations/v1.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from uuid import UUID
+
+from pydantic import BaseModel, Field, PositiveInt
+
+from poprox_concepts.api.recommendations.versions import ProtocolVersions, RecommenderInfo
+from poprox_concepts.domain import Article, InterestProfile
+
+
+class ProtocolModelV1_1(BaseModel):
+    """
+    Version 1.1 of the POPROX protocol added the `recommender` field to `RecommendationResponse`
+    to allow the platform to track which specific recommendation pipeline generated the returned
+    recommendations
+    """
+
+    protocol_version: ProtocolVersions = Field(default=ProtocolVersions.VERSION_1_1, frozen=True)
+
+
+class RecommendationRequestV1(ProtocolModelV1_1):
+    todays_articles: list[Article]
+    past_articles: list[Article]
+    interest_profile: InterestProfile
+    num_recs: PositiveInt
+
+
+class RecommendationResponseV1(ProtocolModelV1_1):
+    recommendations: dict[UUID, list[Article]]
+    recommender: RecommenderInfo | None = Field(default=None)
