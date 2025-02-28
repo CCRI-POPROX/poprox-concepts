de7c9ab52afcf8c41e62346d057d3a28178b6078
Karl Higley <khigley@umn.edu>
2025-02-28 11:46:47 -0500

    Create separate Pydantic models for versions of the API/protocol

---
+++ src/poprox_concepts/api/recommendations/v2.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from pydantic import BaseModel, Field, PositiveInt
+
+from poprox_concepts.api.recommendations.versions import ProtocolVersions, RecommenderInfo
+from poprox_concepts.domain import CandidateSet, InterestProfile, RecommendationList
+
+
+class ProtocolModelV2_0(BaseModel):
+    """
+    Version 2.0 of the POPROX protocol changed the names of types of the fields representing
+    interacted and candidate items, as well as the returned recommendations
+    """
+
+    protocol_version: ProtocolVersions = Field(default=ProtocolVersions.VERSION_2_0, frozen=True)
+
+
+class RecommendationRequestV2(ProtocolModelV2_0):
+    candidates: CandidateSet
+    interacted: CandidateSet
+    interest_profile: InterestProfile
+    num_recs: PositiveInt
+
+
+class RecommendationResponseV2(ProtocolModelV2_0):
+    recommendations: RecommendationList
+    recommender: RecommenderInfo | None = Field(default=None)
