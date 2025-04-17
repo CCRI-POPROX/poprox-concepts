127083c5f30bded7fa825c94cc0e64482a842ddd
Karl Higley <khigley@umn.edu>
2025-04-17 09:14:43 -0400

    Add methods for internal/external account identification

--- src/poprox_concepts/domain/account.py
+++ src/poprox_concepts/domain/account.py
@@ -35,3 +35,24 @@ COMPENSATION_CARD_OPTIONS = [
     "Walmart",
     "Tango",
 ]
+
+INTERNAL_ACCOUNT_SOURCES = [
+    "friends_of_poprox",
+    "team",
+    "test",
+    "kluver",
+]
+
+EXTERNAL_ACCOUNT_SOURCES = [
+    "website",
+    "ff",
+    "ml-volunteers",
+]
+
+
+def is_internal_account(account: Account) -> bool:
+    return account.source is not None and account.source in INTERNAL_ACCOUNT_SOURCES
+
+
+def is_external_account(account: Account) -> bool:
+    return account.source is not None and account.source in EXTERNAL_ACCOUNT_SOURCES
