bd2e0fa75d72d57c40f926d941d004fb036f6cf1
Sris-Ty <srist001@umn.edu>
2025-05-27 13:59:52 -0500

    added article_feedbacks to the interestprofile

--- src/poprox_concepts/domain/profile.py
+++ src/poprox_concepts/domain/profile.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, ConfigDict
 
 from poprox_concepts.domain.account import AccountInterest
 from poprox_concepts.domain.click import Click
+from poprox_concepts.domain.article import Article
 
 
 class InterestProfile(BaseModel):
@@ -13,4 +14,6 @@ class InterestProfile(BaseModel):
     click_history: list[Click]
     click_topic_counts: dict[str, int] | None = None
     click_locality_counts: dict[str, int] | None = None
+    article_feedbacks: dict[UUID, bool] | None = None 
     onboarding_topics: list[AccountInterest]
+  
