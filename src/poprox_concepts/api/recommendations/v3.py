9fb0da4b77cd7910d21b90996c6a3dcfdd1e56d8
Daniel Kluver <kluve018@umn.edu>
2025-06-10 16:21:03 +0000

    add protocol version 3, basic list support.

---
+++ src/poprox_concepts/api/recommendations/v3.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from pydantic import BaseModel, Field, PositiveInt
+
+from poprox_concepts.api.recommendations.versions import ProtocolVersions, RecommenderInfo
+from poprox_concepts.domain.profile import InterestProfile
+from poprox_concepts.domain.recommendation import CandidateSet, RecommendationList
+
+
+class ProtocolModelV3_0(BaseModel):
+    """
+    Version 3.0 of the POPROX protocol changed the return model of a recommendation to return a list of "sections"
+    """
+
+    protocol_version: ProtocolVersions = Field(default=ProtocolVersions.VERSION_3_0, frozen=True)
+
+
+class RecommendationRequestV3(ProtocolModelV3_0):
+    candidates: CandidateSet
+    interacted: CandidateSet
+    interest_profile: InterestProfile
+    num_recs: PositiveInt
+
+
+class RecommendationResponseSection(BaseModel):
+    title: str
+    recommendations: RecommendationList
+
+
+class RecommendationResponseV3(ProtocolModelV3_0):
+    recommendations: list[RecommendationResponseSection]
+    recommender: RecommenderInfo | None = Field(default=None)
