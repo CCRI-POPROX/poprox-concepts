79e80e1eb235e13dfa7ae373ff90a6341c5331d3
Mahamudul42 <hasan181@umn.edu>
2025-06-20 17:43:56 -0500

    removed frequency attribute

--- src/poprox_concepts/domain/experience.py
+++ src/poprox_concepts/domain/experience.py
@@ -6,10 +6,9 @@ from pydantic import BaseModel
 
 class Experience(BaseModel):
     experience_id: UUID | None = None
-    name: str
     recommender_id: UUID
     team_id: UUID
+    name: str
     start_date: date
     end_date: date
     created_at: datetime | None = None
-    frequency: str | None = None
