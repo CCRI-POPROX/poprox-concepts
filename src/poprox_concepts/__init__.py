7717972e7ace6a146b32d0ba564489da78200046
Karl Higley <khigley@umn.edu>
2025-10-28 12:12:53 -0400

    Remove top-level exports of domain concepts

--- src/poprox_concepts/__init__.py
+++ src/poprox_concepts/__init__.py
@@ -1,29 +1,7 @@
 from poprox_concepts import api, domain, internals
-from poprox_concepts.domain import (
-    Account,
-    AccountInterest,
-    Article,
-    ArticlePlacement,
-    CandidateSet,
-    Click,
-    Entity,
-    InterestProfile,
-    Mention,
-    Newsletter,
-)
 
 __all__ = [
     "api",
     "domain",
     "internals",
-    "Account",
-    "AccountInterest",
-    "Article",
-    "CandidateSet",
-    "Click",
-    "Entity",
-    "InterestProfile",
-    "Mention",
-    "ArticlePlacement",
-    "Newsletter",
 ]
