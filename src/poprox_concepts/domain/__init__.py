f3359e8224670097697bbb533c37921d99f4432b
Karl Higley <khigley@umn.edu>
2025-11-06 15:22:53 -0500

    Appease the linter

--- src/poprox_concepts/domain/__init__.py
+++ src/poprox_concepts/domain/__init__.py
@@ -1,4 +1,4 @@
-from poprox_concepts.domain.account import Account, AccountInterest, ConsentLog, EntityType,Subscription
+from poprox_concepts.domain.account import Account, AccountInterest, ConsentLog, EntityType, Subscription
 from poprox_concepts.domain.article import Article, ArticlePackage, ArticlePlacement, Entity, Mention, TopNewsHeadline
 from poprox_concepts.domain.click import Click
 from poprox_concepts.domain.demographics import Demographics
