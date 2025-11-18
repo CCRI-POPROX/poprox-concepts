7e5f48b17e182c790f06e2d63962c7d45211bc0c
Karl Higley <khigley@umn.edu>
2025-11-18 09:56:37 -0500

    Remove the default value from `InterestProfile.entity_interests`
    This default allows downstream behavior in `platform` and `recommender` that can result in communication problems between the two that effectively drop user topical interests on the floor and completely ignore them.

--- src/poprox_concepts/domain/profile.py
+++ src/poprox_concepts/domain/profile.py
@@ -8,14 +8,25 @@ from poprox_concepts.domain.click import Click
 
 
 class InterestProfile(BaseModel):
+    # This allows extra properties to be added to the model that it isn't expecting,
+    # which is useful for allowing the recommender to enrich the interest profile with
+    # additional information (like user embeddings).
+    #
+    # However, allowing extra properties to be added by downstream code means we have to
+    # be careful that anything that we expect to always be present DOES NOT have a default
+    # value in the list of attributes below. Supplying a default makes that field optional,
+    # which means that parsing and validation won't expect it to be present. This can result
+    # in unexpected behavior when renaming attributes, since neither supplying an unexpected
+    # attribute name nor failing to provide an expected attribute name will cause an error
+    # if a default is supplied.
     model_config = ConfigDict(extra="allow")
 
     profile_id: UUID | None = None
+    entity_interests: list[AccountInterest]
     click_history: list[Click]
     click_topic_counts: dict[str, int] | None = None
     click_locality_counts: dict[str, int] | None = None
     article_feedbacks: dict[UUID, bool] | None = None
-    entity_interests: list[AccountInterest] = []
 
     def interests_by_type(self, entity_type: EntityType) -> Iterable[AccountInterest]:
         return (ai for ai in self.entity_interests if ai.entity_type == entity_type)
